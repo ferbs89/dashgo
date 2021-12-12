0e9f1c9a7d25a8135b3fdce56ce5d0439e823885
Fernando Sanches <ferbs89@gmail.com>
2021-12-12 13:50:39 -0300

    Login e dashboard

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,89 @@
+import dynamic from 'next/dynamic';
+import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react';
+import { ApexOptions } from 'apexcharts';
+
+import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+	ssr: false,
+});
+
+const options: ApexOptions = {
+	chart: {
+		toolbar: {
+			show: false,
+		},
+		zoom: {
+			enabled: false,
+		},
+		foreColor: theme.colors.gray[500],
+	},
+	grid: {
+		show: false,
+	},
+	dataLabels: {
+		enabled: false,
+	},
+	tooltip: {
+		enabled: false,
+	},
+	xaxis: {
+		type: 'datetime',
+		axisBorder: {
+			color: theme.colors.gray[600],
+		},
+		axisTicks: {
+			color: theme.colors.gray[600],
+		},
+		categories: [
+			'2021-12-05 00:00:00.000Z',
+			'2021-12-06 00:00:00.000Z',
+			'2021-12-07 00:00:00.000Z',
+			'2021-12-08 00:00:00.000Z',
+			'2021-12-09 00:00:00.000Z',
+			'2021-12-10 00:00:00.000Z',
+			'2021-12-11 00:00:00.000Z',
+		],
+	},
+	fill: {
+		opacity: 0.3,
+		type: 'gradient',
+		gradient: {
+			shade: 'dark',
+			opacityFrom: 0.7,
+			opacityTo: 0.3,
+		}
+	},
+}
+
+const series = [
+	{
+		name: 'series1',
+		data: [31, 120, 10, 28, 61, 18, 109],
+	}
+]
+
+export default function Dashboard() {
+	return(
+		<Flex direction="column" h="100vh">
+			<Header />
+
+			<Flex w="100%" maxWidth={1480} my="6" mx="auto" px="6">
+				<Sidebar />
+
+				<SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+					<Box p="8" bg="gray.800" borderRadius={8} pb="4">
+						<Text fontSize="lg" mb="4">Inscritos da semana</Text>
+						<Chart options={options} series={series} type="area" height={160} />
+					</Box>
+
+					<Box p="8" bg="gray.800" borderRadius={8} pb="4">
+						<Text fontSize="lg" mb="4">Taxa de abertura</Text>
+						<Chart options={options} series={series} type="area" height={160} />
+					</Box>
+				</SimpleGrid>
+			</Flex>
+		</Flex>
+	);
+}
